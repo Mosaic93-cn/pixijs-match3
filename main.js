8509095039a1ef3d520d2a389f64b8d1f59e1853
mosaic93 <2401575237@qq.com>
2023-10-07 20:53:47 +0800

    引入图片资源

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let app= new PIXI.Application({width: 800, height: 700, backgroundColor: 0x808080});//创建pixi实例,背景色0x808080为灰色
+document.getElementById("center").appendChild(app.view);//把pixi视图挂载到页面上
+let sprite = PIXI.Sprite.from('assets/1.png')
+sprite.width = 100;
+sprite.height = 100;
+app.stage.addChild(sprite);
+sprite.x=0;
+sprite.y=0;
+let sprite2 = PIXI.Sprite.from('assets/2.png')
+sprite2.width = 100;
+sprite2.height = 100;
+app.stage.addChild(sprite2);
+sprite2.x=100;
+sprite2.y=0;
 
-const app= new PIXI.Application({width: 800, height: 600, backgroundColor: 0x1099bb});
-document.body.appendChild(app.view);
