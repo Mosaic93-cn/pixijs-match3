2697f79d57fdb5886a7913d0ba1d614778bd1e0c
mosaic93 <2401575237@qq.com>
2023-10-08 20:11:37 +0800

    更新main.js 添加创造和放置方法

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 let app= new PIXI.Application({width: 800, height: 700, backgroundColor: 0x808080});//创建pixi实例,背景色0x808080为灰色
+const te1 = PIXI.Texture.from('assets/1.png');
+const te2 = PIXI.Texture.from('assets/2.png');
+const te3 = PIXI.Texture.from('assets/3.png');
+const te4 = PIXI.Texture.from('assets/4.png');//创建纹理对象
 document.getElementById("center").appendChild(app.view);//把pixi视图挂载到页面上
-let sprite = PIXI.Sprite.from('assets/1.png')
-sprite.width = 100;
-sprite.height = 100;
-app.stage.addChild(sprite);
-sprite.x=0;
-sprite.y=0;
-let sprite2 = PIXI.Sprite.from('assets/2.png')
-sprite2.width = 100;
-sprite2.height = 100;
-app.stage.addChild(sprite2);
-sprite2.x=100;
-sprite2.y=0;
-
+let sprNum=0;//精灵数量
+function createSprite(type){    //方法:创建新精灵并挂载到stage上
+    sprNum++
+    eval("let sprite"+sprNum+";")
+    var str1="sprite"+sprNum+"=new PIXI.Sprite(te"+type+");"
+    var str2="sprite"+sprNum+".width=100;sprite"+sprNum+".height=100;";
+    var str3="app.stage.addChild(sprite"+sprNum+");"
+    eval(str1+str2+str3)
+}
+createSprite(1);
+function setSprite(index,x,y){
+    eval("sprite"+index+".x="+x+";sprite"+index+".y="+y+";")
+}
+setSprite(1,0,100);
+let board_type=new Map();//创建map对象board_type,用于储存棋盘上棋子的样式
+let board_index=new Map();//创建map对象board_index,用于储存棋盘上棋子的id
+for(let x=0;x<7;x++){
+    for(let y=0;y<6;y++){
+        board.set(x+","+y,0)
+    }
+}
+console.log(board);
